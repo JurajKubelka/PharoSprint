dbb6b16942488236f375c6b7bf8472389b162277
Juraj Kubelka <juraj.kubelka@gmail.com>
2017-04-07 13:12:59 -0300

    fix deployment description
    - use echo instead of cat

--- scripts/before-deploy.sh
+++ scripts/before-deploy.sh
@@ -4,5 +4,6 @@
 ( cd scripts && ./build.sh -6cz -n PharoSprint )
 
 # set version and commit id for the bintray configuration
-ONELINE_MESSAGE="$(cat "$TRAVIS_COMMIT_MESSAGE" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' | sed -e 's/"/\\"/g')"
+# note that sed -E part does not work on OS X, as we deploy only on Linux, we do not care
+ONELINE_MESSAGE="$(echo "$TRAVIS_COMMIT_MESSAGE" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' | sed -e 's/"/\\"/g')"
 sed -i.bak -e 's/$Rev\$/'"$REVISION"'/' -e  's/$Commit\$/'"$TRAVIS_COMMIT"'/' -e 's/$CommitMessage\$/'"$ONELINE_MESSAGE"'/' .bintray.json
