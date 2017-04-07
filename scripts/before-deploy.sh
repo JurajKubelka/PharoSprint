cb77d09ca5156a8a86eadcc3a714930f569f3fc4
Juraj Kubelka <juraj.kubelka@gmail.com>
2017-04-07 13:51:05 -0300

    debug sed command

--- scripts/before-deploy.sh
+++ scripts/before-deploy.sh
@@ -5,5 +5,10 @@
 
 # set version and commit id for the bintray configuration
 # note that sed -E part does not work on OS X, as we deploy only on Linux, we do not care
-ONELINE_MESSAGE="$(echo "$TRAVIS_COMMIT_MESSAGE" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' | sed -e 's/"/\\"/g')"
+echo "---"
+echo "$TRAVIS_COMMIT_MESSAGE"
+ONELINE_MESSAGE="$(echo "$TRAVIS_COMMIT_MESSAGE" | sed -e :a -e N -e '$!ba' -e 's/\n/\\n/g' -e 's/"/\\"/g')"
+echo "---"
+echo "$ONELINE_MESSAGE"
+echo "---"
 sed -i.bak -e 's/$Rev\$/'"$REVISION"'/' -e  's/$Commit\$/'"$TRAVIS_COMMIT"'/' -e 's/$CommitMessage\$/'"$ONELINE_MESSAGE"'/' .bintray.json
